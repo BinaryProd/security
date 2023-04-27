58c3fa5102e8f5d1058bf0f77fbaa5173231e192
Ideflop <vi03pl@gmail.com>
2023-04-27 12:32:35 +0200

    add error check

--- 1ueb/aufgabe1.c
+++ 1ueb/aufgabe1.c
@@ -4,15 +4,17 @@
 int main ()
 {
     time_t t;
-
-    time(&t);
     struct tm * timeinfo;
-
-    timeinfo = localtime(&t);
     char buffer[80];
 
+    if (!time(&t)) return -1;
+
+    timeinfo = localtime(&t);
+    if (!timeinfo) return -1;
+
+    if (!strftime(buffer, 80, "%c", timeinfo)) return -1;
+
     printf("With localtime() and strftime:\n");
-    strftime(buffer, 80, "%c", timeinfo);
     printf("%s\n", buffer);
 
     printf("With ctime():\n");
