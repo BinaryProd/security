9f74c19999486c9c6c57b387079fa1cd957c1d14
Ideflop <vi03pl@gmail.com>
2023-05-11 12:43:01 +0200

    add ln hardlink

--- 2ueb/1.sh
+++ 2ueb/1.sh
@@ -4,30 +4,31 @@ cd /tmp || exit 1
 
 temp_file=$(mktemp)
 soft_link="soft_link"
+hardlink="hard_link"
 
 dd if=/dev/urandom of="$temp_file" bs=1M count=1
 
 ls -l "$temp_file" || { echo "Error: ls -l doesn't work for $temp_file"; exit 1; }
 
+ln "$temp_file" "$hardlink"
 ln -s "$temp_file" "$soft_link"
 
+ls -l "$hardlink" || { echo "Error: ls -l doesn't work for $hardlink"; exit 1; }
 ls -l "$soft_link" || { echo "Error: ls -l doesn't work for $soft_link"; exit 1; }
 
-
-tar -c -f "$temp_file".tar.gz "$temp_file" 2> /dev/null
+tar -c -f "$hardlink".tar.gz "$hardlink" 2> /dev/null
 tar -c -f "$soft_link".tar.gz "$soft_link" 2> /dev/null
 
-ls -l "$temp_file".tar.gz
+ls -l "$hardlink".tar.gz
 ls -l "$soft_link".tar.gz
 
 # 1+0 Datensätze ein
 # 1+0 Datensätze aus
-# 1048576 Bytes (1,0 MB, 1,0 MiB) kopiert, 0,0059948 s, 175 MB/s
-# -rw------- 1 idefux idefux 1048576 11. Mai 12:00 /tmp/tmp.6oIK6qxCJg
-# ln: die symbolische Verknüpfung 'soft_link' konnte nicht angelegt werden: Die Datei existiert bereits
-# lrwxrwxrwx 1 idefux idefux 19 11. Mai 11:57 soft_link -> /tmp/tmp.zVOcTpIEkX
-# -rw-r--r-- 1 idefux idefux 1054720 11. Mai 12:00 /tmp/tmp.6oIK6qxCJg.tar.gz
-# -rw-r--r-- 1 idefux idefux 10240 11. Mai 12:00 soft_link.tar.gz
+# 1048576 Bytes (1,0 MB, 1,0 MiB) kopiert, 0,00959434 s, 109 MB/s
+# -rw------- 1 idefux idefux 1048576 11. Mai 12:39 /tmp/tmp.ms7op6mqwJ
+# lrwxrwxrwx 1 idefux idefux 19 11. Mai 12:39 soft_link -> /tmp/tmp.ms7op6mqwJ
+# -rw-r--r-- 1 idefux idefux 1054720 11. Mai 12:39 hard_link.tar.gz
+# -rw-r--r-- 1 idefux idefux 10240 11. Mai 12:39 soft_link.tar.gz
 
 # When the original file is archived, it stores the actual data in the archive. 
 # On the other hand, when the symbolic link is archived, it stores the path to the original file in the archive, not the actual data.
